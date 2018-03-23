e87c2f433175909a82da5b921a4a7e08e007d371
hiy2259 <hiy2259@me.com>
2018-03-23 10:03:51 +0900

    AOP Advice 클래스 추가 - around

--- src/myspring/user/test/DBTest.java
+++ src/myspring/user/test/DBTest.java
@@ -1,5 +1,8 @@
 package myspring.user.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,21 +16,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import myspring.user.service.UserService;
 import myspring.user.vo.DeptVO;
 import myspring.user.vo.StudentVO;
 import myspring.user.vo.UserVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations="classpath:config/beans.xml")
+@ContextConfiguration(locations = "classpath:config/beans.xml")
 public class DBTest {
 
 	@Autowired
 	DataSource dataSource;
-	
+
 	@Autowired
 	SqlSession sqlSession;
-	
-	@Test @Ignore
+
+	@Autowired
+	UserService userService;
+
+	@Test
+	@Ignore
 	public void connection() {
 		try {
 			System.out.println(dataSource.getConnection());
@@ -36,8 +44,9 @@ public class DBTest {
 			e.printStackTrace();
 		}
 	}
-	
-	@Test @Ignore
+
+	@Test
+	@Ignore
 	public void sqlsession() {
 		System.out.println(sqlSession.getConnection());
 		UserVO user = new UserVO("yongsnim", "yongs", "남", "서울");
@@ -48,12 +57,19 @@ public class DBTest {
 			System.out.println(userVO);
 		}
 	}
-	
+
 	@Test
+	@Ignore
 	public void student() {
-		StudentVO student = 
-				new StudentVO(1018, "yongs", 34, "1학년", "주간", new DeptVO(20));
+		StudentVO student = new StudentVO(1018, "yongs", 34, "1학년", "주간", new DeptVO(20));
 		int cnt = sqlSession.update("studentNS.insertStudent", student);
 		System.out.println("등록된 건수 " + cnt);
+	}
+
+	@Test
+	public void service() {
+		UserVO user = userService.getUser("gildong");
+		System.out.println(user);
+		assertThat(user.getName(), is("홍길동"));
 	}
 }
